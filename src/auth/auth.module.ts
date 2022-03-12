c1307f80c1cad6580eaf68eee0c4cc36444e0624
Gromov Vlad <vikkiners@yandex.com>
2022-03-12 21:59:51 +0300

    linter settings

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,8 @@
+import {Module} from '@nestjs/common';
+import {RegisterController} from './controllers/register.controller';
+
+@Module({
+  controllers: [RegisterController]
+})
+export class AuthModule {
+}
